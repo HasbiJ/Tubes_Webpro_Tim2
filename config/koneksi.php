ae1a6a3bfd58a8315d10bd9c5227fad1eee58125
mahendrawinatakusuma <kusumamahendra432@gmail.com>
2026-06-29 22:11:37 +0700

    fix: bersihkan total file koneksi database

--- config/koneksi.php
+++ config/koneksi.php
@@ -10,9 +10,3 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-<<<<<<< HEAD
-?>
-=======
-?>
->>>>>>> fb0f6e35f7a46b0cd04842f6dc9b611878c5433b
